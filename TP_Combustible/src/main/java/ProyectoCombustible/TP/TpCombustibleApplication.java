0e55b3a9d71e7dddd2a618f5b5faa73d56fe794c
Inaki <rully.urbizu@gmail.com>
2024-09-27 19:42:18 -0300

    creo el proyecto

---
+++ TP_Combustible/src/main/java/ProyectoCombustible/TP/TpCombustibleApplication.java
@@ -0,0 +1,13 @@
+package ProyectoCombustible.TP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpCombustibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpCombustibleApplication.class, args);
+	}
+
+}
---
+++ TP_Combustible/src/test/java/ProyectoCombustible/TP/TpCombustibleApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoCombustible.TP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpCombustibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
