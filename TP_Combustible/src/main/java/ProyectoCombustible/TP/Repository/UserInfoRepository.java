713a7db50a224aa3e4dbad7bf43dc9993c3b0621
Inaki <rully.urbizu@gmail.com>
2024-09-27 20:00:55 -0300

    agrego filter y repository

---
+++ TP_Combustible/src/main/java/ProyectoCombustible/TP/Repository/UserInfoRepository.java
@@ -0,0 +1,15 @@
+package ProyectoCombustible.TP.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ciu.jwt.model.UserInfo;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+
+	Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+	
+}
