a1073fd5b21b9fe01d90ded7310cd635ffaac7e6
Inaki <rully.urbizu@gmail.com>
2024-11-27 11:13:03 -0300

    restablecer contrasenia

---
+++ src/main/java/ar/edu/unq/grupo4/combustible/dto/ContraseniaDto.java
@@ -0,0 +1,23 @@
+package ar.edu.unq.grupo4.combustible.dto;
+
+public class ContraseniaDto {
+	private String email;
+    private String nuevaContrasenia;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNuevaContrasenia() {
+		return nuevaContrasenia;
+	}
+	public void setNuevaContrasenia(String nuevaContrasenia) {
+		this.nuevaContrasenia = nuevaContrasenia;
+	}
+    
+	public ContraseniaDto() {
+		
+	}
+
+}
